7fe16f9f67b452eeb603c67dd020e183861b6aba
Tiago Santos <santos.tiago@gmail.com>
2020-07-30 09:14:35 +0200

    Importer of indicators

--- app/services/import_indicators.rb
+++ app/services/import_indicators.rb
@@ -4,6 +4,16 @@ class ImportIndicators
     ActiveRecord::Base.transaction do
       puts '>>> IMPORTING INDICATORS <<<'
 
+      file = Rails.root.join('db/files/indicators/indicators.json').open
+      data = JSON.load file
+
+      data['group'].each do |k, v|
+        category = Category.find_or_create_by! name: v
+        layer = data['indicator']["#{k}"]
+        Layer.find_or_create_by! name: layer, category: category
+        Indicator.find_or_create_by! name: layer
+      end
+
       puts '>>> FINISHED IMPORTING INDICATORS <<<'
     end
   end
