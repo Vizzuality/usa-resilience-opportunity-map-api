c8c6ce230ecc9d30133cadf5b0b143c05670fc63
Tiago Santos <santos.tiago@gmail.com>
2020-10-23 19:43:53 +0200

    Added cache control to the vector tiles

--- app/controllers/api/v1/geometries_controller.rb
+++ app/controllers/api/v1/geometries_controller.rb
@@ -3,7 +3,15 @@ module Api
     class GeometriesController < ApiController
       def tiles
         tile = Geometry.vector_tiles params[:z], params[:x], params[:y], params[:level], params[:'parent-id']
+        set_cache_control_headers
         send_data tile
+      end
+
+      private
+
+      def set_cache_control_headers(max_age = 1.month.to_s)
+        response.headers['Cache-Control'] = 'public, no-cache'
+        response.headers['Surrogate-Control'] = "max-age=#{max_age}"
       end
     end
   end
