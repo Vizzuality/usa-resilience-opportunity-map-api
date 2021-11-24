a11d8f49753e7d0e66db235b3ab45f2e156e6e56
CV-Gate <miguel.mendoza@vizzuality.com>
2021-11-24 11:58:37 +0100

    adds digital ocean config

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,6 @@ require_relative 'application'
 
 # Initialize the Rails application.
 Rails.application.initialize!
+
+  # Digital Ocean config
+config.hosts << "us-resilience-api-z88pi.ondigitalocean.app"
