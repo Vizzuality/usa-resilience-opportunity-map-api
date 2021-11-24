ef5d4d8d7336648c20fc41d9cc89835e8e5770fc
CV-Gate <miguel.mendoza@vizzuality.com>
2021-11-24 16:16:06 +0100

    adds digital ocean config

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -28,6 +28,9 @@ Rails.application.configure do
     config.cache_store = :null_store
   end
 
+  # Digital Ocean config
+  config.hosts.clear
+
   # Store uploaded files on the local file system (see config/storage.yml for options).
   config.active_storage.service = :local
 
