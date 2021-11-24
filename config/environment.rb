89d728dba64fbaf6c2f5b17b02fa1098e841805e
CV-Gate <miguel.mendoza@vizzuality.com>
2021-11-24 16:07:00 +0100

    adds digital ocean config

--- config/environment.rb
+++ config/environment.rb
@@ -3,6 +3,3 @@ require_relative 'application'
 
 # Initialize the Rails application.
 Rails.application.initialize!
-
-  # Digital Ocean config
-config.hosts << "us-resilience-api-z88pi.ondigitalocean.app"
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -86,6 +86,8 @@ Rails.application.configure do
     config.logger    = ActiveSupport::TaggedLogging.new(logger)
   end
 
+  # Digital Ocean config
+  config.hosts.clear
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
