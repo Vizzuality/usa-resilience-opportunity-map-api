2dd24f998bf6fd139803a1c1dc80195471531d66
CV-Gate <miguel.mendoza@vizzuality.com>
2021-11-24 16:24:54 +0100

    removes docker

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -87,7 +87,7 @@ Rails.application.configure do
   end
 
   # Digital Ocean config
-  config.hosts.clear
+  # config.hosts.clear
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
